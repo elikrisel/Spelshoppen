c85ac483b690b4806146fa96621d3621df2ede76
Kristofer Sell <kristoferelissell@gmail.com>
2026-01-16 11:07:50 +0100

    Settings things up in DbContext before migration

--- Models/MyDbContext.cs
+++ Models/MyDbContext.cs
@@ -4,5 +4,20 @@ namespace Spelshoppen.Models;
 
 public class MyDbContext : DbContext
 {
-    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductItem> ProductItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductGenre> ProductGenres { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("Spelshoppen");
+        //GenreId och ProductId tillsammans är PK
+        modelBuilder.Entity<ProductGenre>().HasKey(pg => new { pg.GenreId, pg.ProductId });
+        //Sätter precision för priset
+        modelBuilder.Entity<ProductItem>().Property(p => p.Price).HasPrecision(18, 2);
+        
+    }
 }
